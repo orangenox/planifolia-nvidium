4d029947f5cae2c73a41b28691f3ad3f3f4bc50b
tildejustin <spacepepper3.14@gmail.com>
2024-05-07 09:27:02 -0400

    fix slider out of bounds fix making gamma reset to one in 1.20.5+ by implementing it as a mixin in rendering instead of in the callback

---
+++ src/main/java/dev/tildejustin/planifolia/mixin/SliderWidgetMixin.java
@@ -0,0 +1,26 @@
+package dev.tildejustin.planifolia.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.client.gui.widget.SliderWidget;
+import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(SliderWidget.class)
+public abstract class SliderWidgetMixin {
+    @Dynamic
+    @ModifyExpressionValue(
+            method = {
+                    /* 1.20.3+ */ "renderWidget",
+                    // class_357 -> SliderWidget
+                    // method_48579 -> drawButton
+                    // class_332 -> MatrixStack
+                    /* 1.20-1.20.2 */ "Lnet/minecraft/class_357;method_48579(Lnet/minecraft/class_332;IIF)V",
+                    // class_4587 -> DrawContext
+                    /* 1.19-1.19.4 */ "Lnet/minecraft/class_357;method_48579(Lnet/minecraft/class_4587;IIF)V"
+            }, at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/widget/SliderWidget;value:D"), require = 1, allow = 1
+    )
+    private double keepSliderInBounds(double original) {
+        return MathHelper.clamp(original, 0, 1);
+    }
+}
